097915c53b80a335ee3cef881f7ace37601a72df
Purukitto <purukitto@gmail.com>
2019-08-07 14:32:18 +0530

    :art: File structure done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:yuso/blocprovs/example-bloc-prov.dart';
+import 'package:yuso/blocs/example-bloc.dart';
+import 'package:yuso/theme/style.dart';
+import 'package:yuso/screens/home/homescreen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Home(),
-    ),
-  );
+  runApp(ExampleApp());
+}
+
+class ExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ExampleProvider(
+      bloc: ExampleBloc(),
+      child: MaterialApp(
+        title: 'ExampleApp',
+        theme: appTheme(),
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+          "/": (BuildContext context) => ExScreen1(),
+          "/ExScreen2": (BuildContext context) => ExScreen2(),
+        },
+      ),
+    );
+  }
 }
