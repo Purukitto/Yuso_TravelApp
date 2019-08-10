167b0fbee2d91a096d51268588e15b02784268b9
aniketkishore100 <aniketkishore100@gmail.com>
2019-08-10 23:16:52 +0530

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,44 @@ import 'package:yuso/blocprovs/example-bloc-prov.dart';
 import 'package:yuso/blocs/example-bloc.dart';
 import 'package:yuso/theme/style.dart';
 import 'package:yuso/screens/home/homescreen.dart';
+import 'screens/index.dart';
+import 'screens/auth/register.dart';
+
 
 void main() {
-  runApp(ExampleApp());
+  runApp(YusoStart());
 }
 
-class ExampleApp extends StatelessWidget {
+/*class YusoStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ExampleProvider(
       bloc: ExampleBloc(),
       child: MaterialApp(
         title: 'ExampleApp',
-        theme: appTheme(),
+        //theme: appTheme(),
         initialRoute: '/',
         routes: <String, WidgetBuilder>{
-          "/": (BuildContext context) => ExScreen1(),
+          "/": (BuildContext context) => Index(),
         },
       ),
     );
+  }
+}
+
+ISSUE: Probably BLOC preventing render or something. Check
+*/
+
+class YusoStart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: MainRegisterComp(),
+      initialRoute: '/',
+      routes: <String , WidgetBuilder>{
+      '/' : (BuildContext context) => Index()
+      },
+    );
+    
   }
 }
