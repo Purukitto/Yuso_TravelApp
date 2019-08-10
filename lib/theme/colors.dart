37a05b68ba42f3a2df0f649c6d438071db27f926
Purukitto <purukitto@gmail.com>
2019-08-10 22:13:12 +0530

    :sparkles: Colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF2A2F43),
+    100: Color(0xFF2A2F43),
+    200: Color(0xFF2A2F43),
+    300: Color(0xFF2A2F43),
+    400: Color(0xFF2A2F43),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF2A2F43),
+    700: Color(0xFF2A2F43),
+    800: Color(0xFF2A2F43),
+    900: Color(0xFF2A2F43),
+  },
+);
+const int _blackPrimaryValue = 0xFF2A2F43;
+
+const MaterialColor primaryWhite = MaterialColor(
+  _whitePrimaryValue,
+  <int, Color>{
+    50: Color(0xFFf4f4f2),
+    100: Color(0xFFf4f4f2),
+    200: Color(0xFFf4f4f2),
+    300: Color(0xFFf4f4f2),
+    400: Color(0xFFf4f4f2),
+    500: Color(_whitePrimaryValue),
+    600: Color(0xFFf4f4f2),
+    700: Color(0xFFf4f4f2),
+    800: Color(0xFFf4f4f2),
+    900: Color(0xFFf4f4f2),
+  },
+);
+const int _whitePrimaryValue = 0xFFf4f4f2;
+
+const MaterialColor primaryYellow = MaterialColor(
+  _yellowPrimaryValue,
+  <int, Color>{
+    50: Color(0xFFFFDE73),
+    100: Color(0xFFFFDE73),
+    200: Color(0xFFFFDE73),
+    300: Color(0xFFFFDE73),
+    400: Color(0xFFFFDE73),
+    500: Color(_yellowPrimaryValue),
+    600: Color(0xFFFFDE73),
+    700: Color(0xFFFFDE73),
+    800: Color(0xFFFFDE73),
+    900: Color(0xFFFFDE73),
+  },
+);
+const int _yellowPrimaryValue = 0xFFFFDE73;
+
+const MaterialColor accentYellow = MaterialColor(
+  _accentYellowValue,
+  <int, Color>{
+    50: Color(0xFFf8e8c6),
+    100: Color(0xFFf8e8c6),
+    200: Color(0xFFf8e8c6),
+    300: Color(0xFFf8e8c6),
+    400: Color(0xFFf8e8c6),
+    500: Color(_accentYellowValue),
+    600: Color(0xFFf8e8c6),
+    700: Color(0xFFf8e8c6),
+    800: Color(0xFFf8e8c6),
+    900: Color(0xFFf8e8c6),
+  },
+);
+const int _accentYellowValue = 0xFFf8e8c6;
