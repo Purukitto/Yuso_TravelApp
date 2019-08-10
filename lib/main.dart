fa388352db6393bae0915ef26236b38a92154507
Purukitto <purukitto@gmail.com>
2019-08-10 23:01:41 +0530

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:yuso/blocprovs/example-bloc-prov.dart';
-import 'package:yuso/blocs/example-bloc.dart';
 import 'package:yuso/theme/style.dart';
 import 'package:yuso/screens/home/homescreen.dart';
 
@@ -12,16 +9,13 @@ void main() {
 class ExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ExampleProvider(
-      bloc: ExampleBloc(),
-      child: MaterialApp(
-        title: 'ExampleApp',
-        theme: appTheme(),
-        initialRoute: '/',
-        routes: <String, WidgetBuilder>{
-          "/": (BuildContext context) => ExScreen1(),
-        },
-      ),
+    return new MaterialApp(
+      title: 'Yuso',
+      theme: appTheme(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        "/": (BuildContext context) => ExScreen1(),
+      },
     );
   }
 }
