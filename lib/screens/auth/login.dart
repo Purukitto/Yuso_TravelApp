c8502353feb25e4827dff83823884c0fc029a4d6
Purukitto <purukitto@gmail.com>
2019-08-11 21:31:48 +0530

    .

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -11,8 +11,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-
-    
     final logo = Hero(
       tag: 'hero',
       child: CircleAvatar(
@@ -112,38 +110,34 @@ class _LoginPageState extends State<LoginPage> {
       ),
       onPressed: () {},
     );
-  
 
     return new GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-      
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: <Widget>[
-            SizedBox(height: 40.0),
-            logo,
-            SizedBox(height: 70.0),
-            email,
-            SizedBox(height: 8.0),
-            password,
-            forgotLabel,
-            SizedBox(height: 0.0),
-            loginButton,
-            SizedBox(height: 0.0),
-            googleSignIn,
-            SizedBox(height: 15.0),
-            signin
-          ],
-        ),
-      ),
-      )
-    );
-
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.only(left: 24.0, right: 24.0),
+              children: <Widget>[
+                SizedBox(height: 40.0),
+                logo,
+                SizedBox(height: 70.0),
+                email,
+                SizedBox(height: 8.0),
+                password,
+                forgotLabel,
+                SizedBox(height: 0.0),
+                loginButton,
+                SizedBox(height: 0.0),
+                googleSignIn,
+                SizedBox(height: 15.0),
+                signin
+              ],
+            ),
+          ),
+        ));
   }
 }
