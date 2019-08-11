db54a34ab93b1e34b9ee6acb4434f907665f451c
aniketkishore100 <aniketkishore100@gmail.com>
2019-08-11 16:33:30 +0530

    SplashLoadingTimer

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MaterialApp(
-  theme: ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
-  debugShowCheckedModeBanner: false,
-  home: SplashScreenComp(),
-)
-);
+      theme:
+          ThemeData(primaryColor: Colors.red, accentColor: Color(0xFFf4f4f2)),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenComp(),
+    ));
 
 class SplashScreenComp extends StatefulWidget {
   @override
@@ -13,6 +14,12 @@ class SplashScreenComp extends StatefulWidget {
 }
 
 class _SplashScreenCompState extends State<SplashScreenComp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5),()=> print("Next Screen Now"));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,29 +30,35 @@ class _SplashScreenCompState extends State<SplashScreenComp> {
             decoration: BoxDecoration(color: Color.fromRGBO(255, 222, 115, 1)),
           ),
           Column(
-
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Padding(padding: EdgeInsets.all(80),),
-              
+              Padding(
+                padding: EdgeInsets.all(100),
+              ),
               CircleAvatar(
                 backgroundColor: Colors.transparent,
-                
                 radius: 200.0,
-                  
-                  child: Image.asset("assets/logo1.png"),
-                  
-                ),
-              Text(
-                
-                    "Your Daily Travel Companion", style: TextStyle(
-                      color: Color(0xFFf4f4f2), 
+                child: Image.asset("assets/logo1.png"),//correct the location of the image.
+              ),
+              Container(
+                height: 50,
+                padding: EdgeInsets.only(bottom: 13),
+                child: CircularProgressIndicator(),
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                  child: Text(
+                    "Your Daily Travel Companion",
+                    style: TextStyle(
+                      color: Color(0xFFf4f4f2),
                       fontSize: 20.0,
-                      fontWeight: FontWeight.bold
-                      ),
-                  ),
+                      fontFamily: 'OpenSans-Light',
+                      //fontWeight: FontWeight.bold),
+                    ),
+                  ))
             ],
           ),
+         
         ],
       ),
     );
