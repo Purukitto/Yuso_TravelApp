ff5deea83858d6d16fdb06fdbfb58d0c3deed383
Pranjal  Datta <pranjaldatta99@gmail.com>
2019-08-26 21:27:00 +0530

    username changes

--- server/assets/keys.js
+++ server/assets/keys.js
@@ -2,7 +2,7 @@ var mongoUsername = ""; //enter mongoUsername here
 var mongoPass = "";   //enter pass
 
 //replace <password> with password for given username
-const mongouri = "mongodb+srv://pranjaldatta:"+mongoPass+"@yusodb-qus7o.mongodb.net/test?retryWrites=true&w=majority";
+const mongouri = "mongodb+srv://<username>:"+mongoPass+"@yusodb-qus7o.mongodb.net/test?retryWrites=true&w=majority";
 
 
 
