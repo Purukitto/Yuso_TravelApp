47e0a625f42376fdb7a70eaa4c1859f172ba6aa5
Mashu Duek <mashuduek@gmail.com>
2019-08-30 14:56:35 -0400

    refactor router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,72 +1,54 @@
-var express = require('express');
-var router = express.Router();
-var axios = require('axios');
-var users = require("../models/userAuth");
-var mongoose = require('mongoose');
-
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const users = require('../models/userAuth');
+const mongoose = require('mongoose');
 
 //index router
-router.get('/' , (req,res) => {
+router.get('/', (req, res) => {
     console.log("Recived home request");
     res.send("Hello world!");
-
-    
-})
+});
 
 //demo route
 router.get('/home', (req, res)=>{
     console.log("home called");
     res.send("hello");
-} )
-
+});
 
 //route that handles register requests
-router.post("/register" , (req , res) => {
+router.post('/register', (req, res) => {
 
     //create a newUser document (ref: userAuth.js)
+    const { name, username, email, password, ph } = req.body;
     const newUser = new users({
-        name : req.body.name,
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password,
-        ph : req.body.ph,
-    })
+        name, username, email, password, ph
+    });
 
     //save it to mongoDB and define callbacks 
     newUser.save((err) => {
-        if(err){
-            console.log("Error: " , err);
+        if (err) {
+            console.log("Error: ", err);
             res.statusCode = 1;
             res.body = '';
             res.send(res);
-        }else{
+        } else {
             console.log(newUser);
             res.send("Successfully added");
         }
-    })
-
-})
-
-router.get("/signin" , (req ,res) => {   //ISSUE: NO SECURITY. FIX!!!!!
-    var email = req.query.email;
-    var password = req.query.password;
-    users.find({password: password},(err , document) => {
-        if(err != null){
-            res.send("false");
-        }else{
-            if(document[0]){
-             if(document[0].email == email){              
-                 res.send("true");
-             }else{
-                 res.send("false");
-             }
-        }else{
-            res.send("false");
-        }
-    }
-    })
+    });
 });
 
+router.get('/signin', (req, res) => {   //ISSUE: NO SECURITY. FIX!!!!!
+    const { email, password } = req.query;
+    users.find({ password }, (err, document) => {
+        if (err != null) {
+            res.send("false");
+        } else {
+            (document[0] && document[0].email == email) ?          
+                res.send("true") : res.send("false");
+        }
+    });
+});
 
-
-module.exports = router
+module.exports = router;
