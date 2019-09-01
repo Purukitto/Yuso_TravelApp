b0bfa20bc3f5c7faf229e18d3bb4e1d5d5b4ed52
Restyled.io <commits@restyled.io>
2019-09-01 16:13:22 +0000

    Restyled by prettier

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,54 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const users = require('../models/userAuth');
-const mongoose = require('mongoose');
+const axios = require("axios");
+const users = require("../models/userAuth");
+const mongoose = require("mongoose");
 
 //index router
-router.get('/', (req, res) => {
-    console.log("Recived home request");
-    res.send("Hello world!");
+router.get("/", (req, res) => {
+  console.log("Recived home request");
+  res.send("Hello world!");
 });
 
 //demo route
-router.get('/home', (req, res)=>{
-    console.log("home called");
-    res.send("hello");
+router.get("/home", (req, res) => {
+  console.log("home called");
+  res.send("hello");
 });
 
 //route that handles register requests
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
+  //create a newUser document (ref: userAuth.js)
+  const { name, username, email, password, ph } = req.body;
+  const newUser = new users({
+    name,
+    username,
+    email,
+    password,
+    ph
+  });
 
-    //create a newUser document (ref: userAuth.js)
-    const { name, username, email, password, ph } = req.body;
-    const newUser = new users({
-        name, username, email, password, ph
-    });
-
-    //save it to mongoDB and define callbacks 
-    newUser.save((err) => {
-        if (err) {
-            console.log("Error: ", err);
-            res.statusCode = 1;
-            res.body = '';
-            res.send(res);
-        } else {
-            console.log(newUser);
-            res.send("Successfully added");
-        }
-    });
+  //save it to mongoDB and define callbacks
+  newUser.save(err => {
+    if (err) {
+      console.log("Error: ", err);
+      res.statusCode = 1;
+      res.body = "";
+      res.send(res);
+    } else {
+      console.log(newUser);
+      res.send("Successfully added");
+    }
+  });
 });
 
-router.get('/signin', (req, res) => {   //ISSUE: NO SECURITY. FIX!!!!!
-    const { email, password } = req.query;
-    users.find({ password }, (err, document) => {
-        if (err != null) {
-            res.send("false");
-        } else {
-            (document[0] && document[0].email == email) ?          
-                res.send("true") : res.send("false");
-        }
-    });
+router.get("/signin", (req, res) => {
+  //ISSUE: NO SECURITY. FIX!!!!!
+  const { email, password } = req.query;
+  users.find({ password }, (err, document) => {
+    if (err != null) {
+      res.send("false");
+    } else {
+      document[0] && document[0].email == email
+        ? res.send("true")
+        : res.send("false");
+    }
+  });
 });
 
 module.exports = router;
